a3ca63e5b6ffc6f63bedf15613971bf3c4e68667
Aaron Heckmann <aaron.heckmann+github@gmail.com>
2010-11-01 20:34:57 -0400

    add noop fn

--- gmp.cc
+++ gmp.cc
@@ -15,6 +15,11 @@ using namespace node;
 // http://github.com/embedthis/packages/blob/a0123bc7a4728dd1b4eec012d46f2bf45ae3c8f0/php/php-5.3.2/ext/gmp/gmp.c
 
 Handle<Value>
+noop(const Arguments &args) {
+  return Undefined();
+}
+
+Handle<Value>
 add(const Arguments &args) {
   HandleScope scope;
   if (args[0]->IsNumber() && args[1]->IsNumber()) {
