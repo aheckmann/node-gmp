02f19b5e2bba0d1be6ed2969d28ed9527f446a81
Aaron Heckmann <aaron.heckmann+github@gmail.com>
2010-11-07 08:59:45 -0500

    add failing object tests

--- test/index.js
+++ test/index.js
@@ -14,22 +14,34 @@ assert.equal("1001", new gmp.Int("1").add(1000).toString());
 assert.equal("1001", new gmp.Int(1).add("1000").toString());
 assert.equal("1001", new gmp.Int("1").add("1000").toString());
 
-var err;
-try {
-  new gmp.Int("fail");
-} catch (er) {
-  err = er;
-}
-assert.equal("bad argument", err.message);
-err = null;
 
-var thirtyThree = new gmp.Int(33);
-try {
-  thirtyThree.add("fail");
-} catch (er) {
-  err = er;
-}
-assert.equal("bad argument", err.message);
+; [ "fail"
+  , {}
+  , []
+  , /regx/
+  , function(){}
+  , NaN
+  ].forEach(function(bad){
+      console.log("testing " + bad);
+      var err = {};
+      try {
+        new gmp.Int(bad);
+      } catch (er) {
+        err = er;
+      }
+      assert.equal("bad argument", err.message);
+
+      err = {};
+      var thirtyThree = new gmp.Int(33);
+      try {
+        thirtyThree.add(bad);
+      } catch (er) {
+        err = er;
+      }
+      assert.equal("bad argument", err.message);
+  })
+;
+
 
 
 assert.equal( "2233445598765432112345678909876543212345728034872035834572034572803457802435728034578023457023580250"
